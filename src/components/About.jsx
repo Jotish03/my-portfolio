3ccbe00387c208b55e03097ecc585cedf892f6de
jotish03 <jotishsharma03@gmail.com>
2024-02-16 13:00:27 +0530

    resume updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Tilt from "react-tilt";
 import { color, motion } from "framer-motion";
-import JotishResumePDF from '../assets/JotishResume.pdf'
+import JotishResumePDF from "../assets/JotishResume.pdf";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt className="xs:w-[250px] w-full">
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
       <div
         options={{
@@ -19,15 +19,15 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450,
         }}
-        className='bg-[#000000] rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        className="bg-[#000000] rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt="web-development"
+          className="w-16 h-16 object-contain"
         />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
+        <h3 className="text-white text-[20px] font-bold text-center">
           {title}
         </h3>
       </div>
@@ -45,24 +45,35 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-A motivated designer and developer with experience creating custom websites through ReactJS, SCSS, Tailwind CSS and more. Strong collaboration skills and proven history of application development.
+        A motivated designer and developer with experience creating custom
+        websites through ReactJS, SCSS, Tailwind CSS and more. Strong
+        collaboration skills and proven history of application development.
       </motion.p>
       <div className="mt-10">
-    
-    <button  class="bg-[#00ff00] hover:bg-white hover:text-black text-black font-bold py-2 px-4 rounded inline-flex items-center">
-  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-  <span>  <a href={JotishResumePDF} download="JothResume.pdf">Download CV</a></span>
-</button>
-    </div>
- 
-      <div className='mt-20 flex flex-wrap gap-10'>
+        <button class="bg-[#00ff00] hover:bg-white hover:text-black text-black font-bold py-2 px-4 rounded inline-flex items-center">
+          <svg
+            class="fill-current w-4 h-4 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+          </svg>
+          <span>
+            {" "}
+            <a href={JotishResumePDF} download="JothResume.pdf">
+              Download CV
+            </a>
+          </span>
+        </button>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
- 
     </>
   );
 };
